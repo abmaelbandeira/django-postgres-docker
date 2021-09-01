87336acb2e33617a08966e43b3ee7603d3a603b4
Abmael Bandeira <abmaelbandeira@hotmail.com>
2021-09-01 08:11:07 -0300

    API Products

--- products/serializers.py
+++ products/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Product
 
 
-class ProductSerializer(serializers.HyperlinkedModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ('id', 'description', 'price')
--- products/views.py
+++ products/views.py
@@ -1,6 +1,4 @@
 from rest_framework import viewsets
-from rest_framework import permissions
-
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -10,4 +8,3 @@ class ProductViewSet(viewsets.ModelViewSet):
     """
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [permissions.IsAuthenticated]
